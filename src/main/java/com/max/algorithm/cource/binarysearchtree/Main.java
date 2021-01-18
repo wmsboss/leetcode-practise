030cccfa162e0494a2aac3706767b2e67ef91d94
Wu Ming Shan <wumingshan@patsnap.com>
2021-01-18 20:53:36 +0800

    Add binary search tree practise

--- src/main/java/com/max/algorithm/cource/binarysearchtree/Main.java
+++ src/main/java/com/max/algorithm/cource/binarysearchtree/Main.java
@@ -1,34 +1,146 @@
 package com.max.algorithm.cource.binarysearchtree;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
 
+//        BST<Integer> bst = new BST<>();
+//        int[] nums = {5, 3, 6, 8, 4, 2};
+//        for(int num: nums)
+//            bst.add(num);
+//
+//        /////////////////
+//        //      5      //
+//        //    /   \    //
+//        //   3    6    //
+//        //  / \    \   //
+//        // 2  4     8  //
+//        /////////////////
+//        bst.preOrder();
+//        System.out.println();
+//
+//        bst.inOrder();
+//        System.out.println();
+//
+//        bst.postOrder();
+//        System.out.println();
+//
+//        bst.preOrderNR();
+//        System.out.println();
+//
+//        bst.levelOrder();
+//        System.out.println();
+
+        // testRemoveMinAndRemoveMax();
+        // testRemove();
+        testBSTSet();
+    }
+
+    public static void testRemoveMinAndRemoveMax() {
         BST<Integer> bst = new BST<>();
-        int[] nums = {5, 3, 6, 8, 4, 2};
-        for(int num: nums)
-            bst.add(num);
+        Random random = new Random();
 
-        /////////////////
-        //      5      //
-        //    /   \    //
-        //   3    6    //
-        //  / \    \   //
-        // 2  4     8  //
-        /////////////////
-        bst.preOrder();
+        int n = 1000;
+
+        // test removeMin
+        for(int i = 0 ; i < n ; i ++)
+            bst.add(random.nextInt(10000));
+
+        ArrayList<Integer> nums = new ArrayList<>();
+        while(!bst.isEmpty())
+            nums.add(bst.removeMin());
+
+        System.out.println(nums);
+        for(int i = 1 ; i < nums.size() ; i ++)
+            if(nums.get(i - 1) > nums.get(i))
+                throw new IllegalArgumentException("Error!");
+        System.out.println("removeMin test completed.");
+
+
+        // test removeMax
+        for(int i = 0 ; i < n ; i ++)
+            bst.add(random.nextInt(10000));
+
+        nums = new ArrayList<>();
+        while(!bst.isEmpty())
+            nums.add(bst.removeMax());
+
+        System.out.println(nums);
+        for(int i = 1 ; i < nums.size() ; i ++)
+            if(nums.get(i - 1) < nums.get(i))
+                throw new IllegalArgumentException("Error!");
+        System.out.println("removeMax test completed.");
+    }
+
+    public static void testRemove() {
+        BST<Integer> bst = new BST<>();
+        Random random = new Random();
+
+        int n = 10000;
+
+        for(int i = 0 ; i < n ; i ++)
+            bst.add(random.nextInt(n));
+
+        // 注意, 由于随机生成的数据有重复, 所以bst中的数据数量大概率是小于n的
+
+        // order数组中存放[0...n)的所有元素
+        Integer[] order = new Integer[n];
+        for( int i = 0 ; i < n ; i ++ )
+            order[i] = i;
+        // 打乱order数组的顺序
+        shuffle(order);
+
+        // 乱序删除[0...n)范围里的所有元素
+        for( int i = 0 ; i < n ; i ++ )
+            if(bst.contains(order[i])){
+                bst.remove(order[i]);
+                System.out.println("After remove " + order[i] + ", size = " + bst.size() );
+            }
+
+        // 最终整个二分搜索树应该为空
+        System.out.println(bst.size());
+    }
+
+    private static void testBSTSet() {
+        System.out.println("Pride and Prejudice");
+
+        ArrayList<String> words1 = new ArrayList<>();
+        if(FileOperation.readFile("pride-and-prejudice.txt", words1)) {
+            System.out.println("Total words: " + words1.size());
+
+            BSTSet<String> set1 = new BSTSet<>();
+            for (String word : words1)
+                set1.add(word);
+            System.out.println("Total different words: " + set1.getSize());
+        }
+
         System.out.println();
 
-        bst.inOrder();
-        System.out.println();
 
-        bst.postOrder();
-        System.out.println();
+        System.out.println("A Tale of Two Cities");
 
-        bst.preOrderNR();
-        System.out.println();
+        ArrayList<String> words2 = new ArrayList<>();
+        if(FileOperation.readFile("a-tale-of-two-cities.txt", words2)){
+            System.out.println("Total words: " + words2.size());
 
-        bst.levelOrder();
-        System.out.println();
+            BSTSet<String> set2 = new BSTSet<>();
+            for(String word: words2)
+                set2.add(word);
+            System.out.println("Total different words: " + set2.getSize());
+        }
+    }
+
+    // 打乱数组顺序
+    private static void shuffle(Object[] arr){
+        for(int i = arr.length - 1 ; i >= 0 ; i --){
+            int pos = (int) (Math.random() * (i + 1));
+            Object t = arr[pos];
+            arr[pos] = arr[i];
+            arr[i] = t;
+        }
     }
 }
