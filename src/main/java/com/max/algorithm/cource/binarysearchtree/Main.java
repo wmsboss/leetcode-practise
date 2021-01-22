ea1fe650ab9361d1aedde3bb207a1c45f2f63993
Wu Ming Shan <wumingshan@patsnap.com>
2021-01-22 11:15:19 +0800

    Add map practise

--- src/main/java/com/max/algorithm/cource/binarysearchtree/Main.java
+++ src/main/java/com/max/algorithm/cource/binarysearchtree/Main.java
@@ -37,7 +37,9 @@ public class Main {
 
         // testRemoveMinAndRemoveMax();
         // testRemove();
-        testBSTSet();
+        //testBSTSet();
+        testSet();
+
     }
 
     public static void testRemoveMinAndRemoveMax() {
@@ -132,6 +134,38 @@ public class Main {
                 set2.add(word);
             System.out.println("Total different words: " + set2.getSize());
         }
+    }
+
+    public static void testSet() {
+        String filename = "pride-and-prejudice.txt";
+
+        BSTSet<String> bstSet = new BSTSet<>();
+        double time1 = testSet(bstSet, filename);
+        System.out.println("BST Set: " + time1 + " s");
+
+        System.out.println();
+
+        LinkedListSet<String> linkedListSet = new LinkedListSet<>();
+        double time2 = testSet(linkedListSet, filename);
+        System.out.println("Linked List Set: " + time2 + " s");
+    }
+
+    private static double testSet(Set<String> set, String filename){
+
+        long startTime = System.nanoTime();
+
+        System.out.println(filename);
+        ArrayList<String> words = new ArrayList<>();
+        if(FileOperation.readFile(filename, words)) {
+            System.out.println("Total words: " + words.size());
+
+            for (String word : words)
+                set.add(word);
+            System.out.println("Total different words: " + set.getSize());
+        }
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
     }
 
     // 打乱数组顺序
