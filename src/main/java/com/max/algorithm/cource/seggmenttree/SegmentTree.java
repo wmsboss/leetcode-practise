2d927697a70d8a08843d45e3a40d33a9bda33c2f
Wu Ming Shan <wumingshan@patsnap.com>
2021-03-18 20:16:35 +0800

    Add Trie practise

--- src/main/java/com/max/algorithm/cource/seggmenttree/SegmentTree.java
+++ src/main/java/com/max/algorithm/cource/seggmenttree/SegmentTree.java
@@ -87,6 +87,35 @@ public class SegmentTree<E> {
         return merger.merge(leftResult, rightResult);
     }
 
+    // 将index位置的值，更新为e
+    public void set(int index, E e){
+        if(index < 0 || index >= data.length)
+            throw new IllegalArgumentException("Index is illegal");
+
+        data[index] = e;
+        set(0, 0, data.length - 1, index, e);
+    }
+
+    // 在以treeIndex为根的线段树中更新index的值为e
+    private void set(int treeIndex, int l, int r, int index, E e){
+        if(l == r){
+            tree[treeIndex] = e;
+            return;
+        }
+
+        int mid = l + (r - l) / 2;
+        // treeIndex的节点分为[l...mid]和[mid+1...r]两部分
+
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+        if(index >= mid + 1)
+            set(rightTreeIndex, mid + 1, r, index, e);
+        else // index <= mid
+            set(leftTreeIndex, l, mid, index, e);
+
+        tree[treeIndex] = merger.merge(tree[leftTreeIndex], tree[rightTreeIndex]);
+    }
+
     @Override
     public String toString(){
         StringBuilder res = new StringBuilder();
